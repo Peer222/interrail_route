36834386a0b361616203c664ff3d8535ecef99e6
Peer Duensing <p.duensing@stud.uni-hannover.de>
2023-05-21 22:41:25 +0200

    csv file creator

--- get_route.py
+++ get_route.py
@@ -5,7 +5,6 @@ import json
 import csv
 
 def extract_route(url):
-    print("URL:", url)
     fp = urllib.request.urlopen(url)
     mybytes = fp.read()
 
@@ -14,9 +13,14 @@ def extract_route(url):
 
     payload = json.loads(re.findall("payload\s=\s(\{[^;]+);", html)[0])
 
-    #print(json.loads(payload["travels"][0]["infoJson"]))
-
     travels = sort_travels(payload["travels"])
+
+    for i in range(len(travels)):
+        travels[i]["infoJson"] = json.loads(travels[i]["infoJson"])
+        travels[i]["optionsJson"] = json.loads(travels[i]["optionsJson"])
+
+        for j in range(len(travels[i]["legs"])):
+            travels[i]["legs"][j]["infoJson"] = json.loads(travels[i]["legs"][j]["infoJson"])
 
     return {
         "name": payload["name"],
@@ -25,11 +29,60 @@ def extract_route(url):
     }
 
 def simplify_route(travels):
-    return {}
+    simplified = []
+    for travel in travels:
+
+        interchanges = ""
+        for l in range(len(travel["legs"]) - 1):
+            interchanges += (travel["legs"][l]["infoJson"]["trainStopStations"][-1]["name"]) + ", "
+
+        if len(interchanges) == 0:
+            interchanges = None
+        else:
+            interchanges = interchanges[:-2]
+
+        date = add_leading_zeros(f'{travel["infoJson"]["date"]["year"]}/{travel["infoJson"]["date"]["month"]}/{travel["infoJson"]["date"]["day"]}')
+        departureTime = add_leading_zeros(f'{travel["infoJson"]["departureTime"]["hours"]}:{travel["infoJson"]["departureTime"]["minutes"]}')
+        arrivalTime = add_leading_zeros(f'{travel["infoJson"]["arrivalTime"]["hours"]}:{travel["infoJson"]["arrivalTime"]["minutes"]}')
+        duration = f'{travel["infoJson"]["arrivalTime"]["hours"] - travel["infoJson"]["departureTime"]["hours"]}:{travel["infoJson"]["arrivalTime"]["minutes"] - travel["infoJson"]["departureTime"]["minutes"]}'
+
+        simplified.append({
+            "timestamp": travel["date"],
+            "from": f'{travel["from"]}, {travel["legs"][0]["infoJson"]["trainStopStations"][0]["country"]}',
+            "to": f'{travel["to"]}, {travel["legs"][-1]["infoJson"]["trainStopStations"][-1]["country"]}',
+            "date": date,
+            "departureTime": departureTime,
+            "arrivalTime": arrivalTime,
+            "duration": add_leading_zeros(correct_negative_minutes(duration)),
+            "interchanges": interchanges
+        })
+    return simplified
 
 def sort_travels(travels):
     travels.sort(key=lambda x : x["date"])
     return travels
+
+def correct_negative_minutes(time:str):
+    hours, minutes = time.split(":")
+    if int(minutes) < 0:
+        return f'{int(hours) - 1}:{60 + int(minutes)}'
+    return time
+
+def add_leading_zeros(x):
+    if ":" in x:
+        fragments = x.split(":")
+    else:
+        fragments = x.split("/")
+
+    for i in range(len(fragments)):
+        if int(fragments[i]) < 10:
+            fragments[i] = "0" + fragments[i]
+
+    if ":" in x:
+        return f'{fragments[0]}:{fragments[1]}'
+    else:
+        return f'{fragments[0]}/{fragments[1]}/{fragments[2]}'
+    
 
 def save_route(data):
 
